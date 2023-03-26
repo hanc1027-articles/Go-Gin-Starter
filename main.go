47fd01e6bcde90835995c1e24020999342cec520
Nelson Hsu <nelsonhsu1027@gmail.com>
2023-03-26 17:39:42 +0800

    Can search the album by id

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
